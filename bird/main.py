07cb2ee55c78e7c6fd33a231a63b892a9a20f353
Léo Graziani <103998540+GrGLeo@users.noreply.github.com>
2023-05-09 22:08:10 +0200

    Added EfficentNetB2 model

--- bird/main.py
+++ bird/main.py
@@ -4,17 +4,29 @@ import time
 from tqdm import tqdm
 
 import torch
+import torchvision
 import torch.nn as nn
 import torch.optim as optim
 
 # Constant
+MODEL_PRETRAINED = True
+MODEL_LOAD = False
 EPOCH = 10
 LEARNING_RATE = 1e-4
 
 # Load data
 train,val,test = data_load()
 
-model = Model()
+# Model instanciation
+if MODEL_PRETRAINED:
+    model = torchvision.models.efficientnet_v2_s(pretrained=True)
+    num_ftrs = model.classifier[-1].in_features
+    model.classifier[-1] = nn.Linear(num_ftrs, 25) 
+else:    
+    model = Model()
+if MODEL_LOAD:
+    model.load_state_dict(torch.load(r"D:\Coding\bird_detection\Bird_Detection\models\934584.pth"))
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(params=model.parameters(),lr=LEARNING_RATE)
 
@@ -86,5 +98,5 @@ for epoch in range(10):
     print(f'Epoch: {epoch} -- loss:{round(epoch_loss,4)} -- acc:{round(epoch_acc,2)} -- val_loss:{round(val_loss,4)} -- val_acc:{round(val_acc,2)}')
 
 # Save model
-path_to_save = rf"D:\Coding\bird_detection\Bird_Detection\models\{str(time.time())[:5]}.pth"
+path_to_save = rf"D:\Coding\bird_detection\Bird_Detection\models\{str(time.time())[-6:]}.pth"
 torch.save(model.state_dict(),path_to_save)
